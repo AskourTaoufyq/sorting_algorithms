24aa226d05b968f2bd8a67cd214bee830743dd77
AskourTaoufyq <kosa477kacm@gmail.com>
2024-03-06 00:48:43 -0800

    sorting algorithms

--- sort.h
+++ sort.h
@@ -24,6 +24,8 @@ void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
+void swap_ints(int *x, int *y);
+void shell_sort(int *array, size_t size);
 
 #endif
 
